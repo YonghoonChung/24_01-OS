d4d7cbcd68a54c1d1e6c38756b02cd38bf985bd3
YonghoonChung <yonghoon1999@gmail.com>
2024-03-24 21:08:27 +0900

    hw02 presetup done

--- hw/hw02/hw2_2.c
+++ hw/hw02/hw2_2.c
@@ -1,1 +1,16 @@
+/*
+ * 2024년 3월 24일
+ * HW 02 assignment by prof.Injung Kim
+ *
+ * Problem : Write a program that copies a file into another.
+ * using open, close, read, write
+ */
 #include <stdio.h>
+
+int main (int argc, char *argv[]){
+	for(int i = 0; i <= argc; i++){
+		printf("argv[%d] = \"%s\"\n",i,argv[i]);
+	}
+	return 0;
+}
+
