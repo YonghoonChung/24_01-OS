bd602b12abe763e28206bf3aad9a56ec39fe903b
Yonghoon Chung <yonghoon1999@gmail.com>
2024-04-29 13:33:45 +0900

    test

---
+++ exam/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <wait.h>
+#include <stdlib.h>
